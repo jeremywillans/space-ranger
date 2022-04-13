0758024c0f631d50ddd6dd847ab0c84572e1492f
Jeremy Willans <jeremy@willans.id.au>
2022-04-13 10:23:55 +0800

    support multi-word bot names

--- src/hears.js
+++ src/hears.js
@@ -6,10 +6,10 @@ module.exports = (framework) => {
   framework.hears(/.*/gim, (bot, trigger) => {
     debug('trigger hears');
     if (bot.room.type === 'group') {
-      const command = trigger.args[1];
+      const command = trigger.message.html.replace(/^.*spark-mention> (.*)<\/p>$/, '$1').split(' ')[0];
       switch (true) {
         case /(hello|help)/i.test(command):
-          bot.reply(trigger.id, `<@personId:${trigger.person.id}>, I will patrol space membership in this quadrant and keep it free from our enemies lead by Evil Emperor Zurg! (aka other org members)  \n - @mention me with \`refresh\` to perform an on-demand space membership patrol  \n - Support can be obtained by joining this [space](https://eurl.io/#3cWKgIWXV)`);
+          bot.reply(trigger.id, `<@personId:${trigger.person.id}>, I will patrol space membership in this quadrant and keep it free from our enemies lead by Evil Emperor Zurg! (aka other org members)  \n - @mention me with \`patrol\` to perform an on-demand space membership patrol  \n - Support can be obtained by joining this [space](https://eurl.io/#3cWKgIWXV)`);
           break;
         case /support/i.test(command):
           bot.reply(trigger.id, `<@personId:${trigger.person.id}>, Please join this [space](https://eurl.io/#3cWKgIWXV) for support!`);
