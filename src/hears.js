46fe668c16932bb96aba4ee24b5fbfe7ce1c2809
Jeremy Willans <jeremy@willans.id.au>
2023-02-23 12:19:55 +1100

    cleanup bot responses

--- src/hears.js
+++ src/hears.js
@@ -6,13 +6,15 @@ module.exports = (framework) => {
   framework.hears(/.*/gim, (bot, trigger) => {
     logger.debug('trigger hears');
     if (bot.room.type === 'group') {
+      let message;
       const command = trigger.message.html.replace(/^.*spark-mention> (.*)<\/p>$/, '$1').split(' ')[0];
       switch (true) {
         case /(hello|help)/i.test(command):
-          bot.reply(trigger.id, `<@personId:${trigger.person.id}>, I will patrol space membership in this quadrant and keep it free from our enemies lead by Evil Emperor Zurg! (aka other org members)  \n - @mention me with \`patrol\` to perform an on-demand space membership patrol  \n - Support can be obtained by joining this [space](https://eurl.io/#3cWKgIWXV)`);
-          break;
-        case /support/i.test(command):
-          bot.reply(trigger.id, `<@personId:${trigger.person.id}>, Please join this [space](https://eurl.io/#3cWKgIWXV) for support!`);
+          message = `<@personId:${trigger.person.id}>,\n\nI will patrol space membership in this quadrant and keep it free from our enemies lead by Emperor Zurg! (aka other org members)  \n - @mention me with \`patrol\` to perform an on-demand space membership patrol`;
+          if (process.env.SUPPORT_EURL) {
+            message += `\n - Support can be obtained by joining this [space](${process.env.SUPPORT_EURL})`;
+          }
+          bot.reply(trigger.id, message);
           break;
         default:
           // Check for Moderation Status
@@ -23,8 +25,17 @@ module.exports = (framework) => {
             syncRoom(framework, bot, trigger.id);
           }
       }
-    } else {
-      bot.say(`Hello ${trigger.person.displayName}!`);
+      return;
+    }
+    // Match on hello/help inputs
+    if (trigger.args[0].match(/(hello|help)/i)) {
+      const identifier = trigger.person.displayName;
+      let message = `Hello ${identifier}!\n\nI can be used to patrol spaces in this quadrant and keep it free from our enemies lead by Emperor Zurg! (aka other org members)\nPlease add me to a group space to get started.\n\nAvailable Resources: \n - [Source Code](https://github.com/jeremywillans/space-ranger)`;
+      if (process.env.SUPPORT_EURL) {
+        message += `\n - [Support Space](${process.env.SUPPORT_EURL})`;
+      }
+      // Send Intro Message
+      bot.say(message);
     }
   });
 };
